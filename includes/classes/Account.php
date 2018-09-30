e3c74798cdcc4afa121349621de5f766d1df417f
rjmcbain <rmcbain01@gmail.com>
2018-09-30 16:33:11 -0600

    preparing for sql

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -1,62 +1,95 @@
 <?php
-  class Account {
+	class Account {
 
-    private $errorArray;
+		private $errorArray;
 
-    public function __construct() {
-      $this->errorArray=array();
-    }
+		public function __construct() {
+			$this->errorArray = array();
+		}
 
-    public function register($un, $fn, $ln, $em, $em2, $pw, $pw2){
-      $this->validateUsername($un);
-      $this->validateFirstname($fn);
-      $this->validateLastname($ln);
-      $this->validateEmails($em, $em);
-      $this->validatePasswords($pw, $pw2);
-    }
+		public function register($un, $fn, $ln, $em, $em2, $pw, $pw2) {
+			$this->validateUsername($un);
+			$this->validateFirstName($fn);
+			$this->validateLastName($ln);
+			$this->validateEmails($em, $em2);
+			$this->validatePasswords($pw, $pw2);
 
-    private function validateUsername($un) {
-      if(strlen($un) > 25 || strlen($un) < 5) {
-        array_push($this->errorArray, "Your username must be between 5 and 25 characters");
-        return;
-      }
+			if(empty($this->errorArray) == true) {
+				//Insert into db
+				return true;
+			}
+			else {
+				return false;
+			}
 
-      // TODO: check if username exists
-    }
-    
-    private function validateFirstname($fn) {
-      if(strlen($fn) > 25 || strlen($fn) < 2) {
-        array_push($this->errorArray, "Your first name must be between 2 and 25 characters");
-        return;
-      }
-    }
-    
-    private function validateLastname($ln) {
-      if(strlen($ln) > 25 || strlen($ln) < 2) {
-        array_push($this->errorArray, "Your last name must be between 2 and 25 characters");
-        return;
-      }
-    }
-    
-    private function validateEmails($em, $em2) {
-      if($em != $em2) {
-        array_push($this->errorArray, "Your emails do not match");
-        return;
-      }
+		}
 
-      if(!filter_var($em, FILTER_VALIDATE_EMAIL)) {
-          array_push($this->errorArray, "Email is invalid");
-          return;
-      }
+		public function getError($error) {
+			if(!in_array($error, $this->errorArray)) {
+				$error = "";
+			}
+			return "<span class='errorMessage'>$error</span>";
+		}
 
-      //TODO: Check for username
-    }
-    
-    private function validatePasswords($pw, $pw2) {
-      if($pw != $pw2) {
-        array_push($this->errorArray, "Your password do not match");
-        return;
-      }
-    }
-  }
+		private function validateUsername($un) {
+
+			if(strlen($un) > 25 || strlen($un) < 5) {
+				array_push($this->errorArray, Constants::$usernameCharacters);
+				return;
+			}
+
+			//TODO: check if username exists
+
+		}
+
+		private function validateFirstName($fn) {
+			if(strlen($fn) > 25 || strlen($fn) < 2) {
+				array_push($this->errorArray, Constants::$firstNameCharacters);
+				return;
+			}
+		}
+
+		private function validateLastName($ln) {
+			if(strlen($ln) > 25 || strlen($ln) < 2) {
+				array_push($this->errorArray, Constants::$lastNameCharacters);
+				return;
+			}
+		}
+
+		private function validateEmails($em, $em2) {
+			if($em != $em2) {
+				array_push($this->errorArray, Constants::$emailsDoNotMatch);
+				return;
+			}
+
+			if(!filter_var($em, FILTER_VALIDATE_EMAIL)) {
+				array_push($this->errorArray, Constants::$emailInvalid);
+				return;
+			}
+
+			//TODO: Check that username hasn't already been used
+
+		}
+
+		private function validatePasswords($pw, $pw2) {
+			
+			if($pw != $pw2) {
+				array_push($this->errorArray, Constants::$passwordsDoNoMatch);
+				return;
+			}
+
+			if(preg_match('/[^A-Za-z0-9]/', $pw)) {
+				array_push($this->errorArray, Constants::$passwordNotAlphanumeric);
+				return;
+			}
+
+			if(strlen($pw) > 30 || strlen($pw) < 5) {
+				array_push($this->errorArray, Constants::$passwordCharacters);
+				return;
+			}
+
+		}
+
+
+	}
 ?>
--- register.php
+++ register.php
@@ -1,32 +1,36 @@
 <?php
-    include("includes/classes/Account.php");
-    $account = new Account();
-    // $account->register();
+	include("includes/classes/Account.php");
+	include("includes/classes/Constants.php");
 
-    include("includes/handlers/register-handler.php");
-    include("includes/handlers/login-handler.php")
-    ?>
+	$account = new Account();
 
-<!DOCTYPE html>
-<html lang="en">
+	include("includes/handlers/register-handler.php");
+	include("includes/handlers/login-handler.php");
+
+	function getInputValue($name) {
+		if(isset($_POST[$name])) {
+			echo $_POST[$name];
+		}
+	}
+?>
+
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Spotify</title>
+	<title>Welcome to Slotify!</title>
 </head>
 <body>
-<div id="inputContainer">
+
+	<div id="inputContainer">
 		<form id="loginForm" action="register.php" method="POST">
 			<h2>Login to your account</h2>
-			<div>
+			<p>
 				<label for="loginUsername">Username</label>
-				<input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. JohnDoe" required>
-			</div>
-			<div>
+				<input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. bartSimpson" required>
+			</p>
+			<p>
 				<label for="loginPassword">Password</label>
 				<input id="loginPassword" name="loginPassword" type="password" placeholder="Your password" required>
-			</div>
+			</p>
 
 			<button type="submit" name="loginButton">LOG IN</button>
 			
@@ -36,40 +40,48 @@
 
 		<form id="registerForm" action="register.php" method="POST">
 			<h2>Create your free account</h2>
-			<div>
+			<p>
+				<?php echo $account->getError(Constants::$usernameCharacters); ?>
 				<label for="username">Username</label>
-				<input id="username" name="username" type="text" placeholder="e.g. John Doe" required>
-			</div>
+				<input id="username" name="username" type="text" placeholder="e.g. bartSimpson" value="<?php getInputValue('username')?>"required>
+			</p>
 
-			<div>
+			<p>
+				<?php echo $account->getError(Constants::$firstNameCharacters); ?>
 				<label for="firstName">First name</label>
-				<input id="firstName" name="firstName" type="text" placeholder="e.g. John" required>
-			</div>
+				<input id="firstName" name="firstName" type="text" placeholder="e.g. Bart" value="<?php getInputValue('firstname')?>"required>
+			</p>
 
-			<div>
+			<p>
+				<?php echo $account->getError(Constants::$lastNameCharacters); ?>
 				<label for="lastName">Last name</label>
-				<input id="lastName" name="lastName" type="text" placeholder="e.g. Doe" required>
-			</div>
+				<input id="lastName" name="lastName" type="text" placeholder="e.g. Simpson" value="<?php getInputValue('lastname')?>"required>
+			</p>
 
-			<div>
+			<p>
+				<?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
+				<?php echo $account->getError(Constants::$emailInvalid); ?>
 				<label for="email">Email</label>
-				<input id="email" name="email" type="email" placeholder="e.g. john@gmail.com" required>
-			</div>
+				<input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email')?>"required>
+			</p>
 
-			<div>
+			<p>
 				<label for="email2">Confirm email</label>
-				<input id="email2" name="email2" type="email" placeholder="e.g. john@gmail.com" required>
-			</div>
+				<input id="email2" name="email2" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email2')?>"required>
+			</p>
 
-			<div>
+			<p>
+				<?php echo $account->getError(Constants::$passwordsDoNoMatch); ?>
+				<?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
+				<?php echo $account->getError(Constants::$passwordCharacters); ?>
 				<label for="password">Password</label>
 				<input id="password" name="password" type="password" placeholder="Your password" required>
-			</div>
+			</p>
 
-			<div>
+			<p>
 				<label for="password2">Confirm password</label>
-				<input id="password2" name="password2" type="password" placeholder="Confirm password" required>
-			</div>
+				<input id="password2" name="password2" type="password" placeholder="Your password" required>
+			</p>
 
 			<button type="submit" name="registerButton">SIGN UP</button>
 			
@@ -77,5 +89,6 @@
 
 
 	</div>
+
 </body>
 </html>
